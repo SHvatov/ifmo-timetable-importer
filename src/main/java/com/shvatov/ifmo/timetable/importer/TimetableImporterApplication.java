c39e1d12a850c69336cbaa40eb1e04f90e87176a
Sergey Hvatov <ishvatov@gmail.com>
2022-09-03 14:44:17 +0300

    Initial commit

---
+++ src/main/java/com/shvatov/ifmo/timetable/importer/TimetableImporterApplication.java
@@ -0,0 +1,13 @@
+package com.shvatov.ifmo.timetable.importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shvatov/ifmo/timetable/importer/TimetableImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shvatov.ifmo.timetable.importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
